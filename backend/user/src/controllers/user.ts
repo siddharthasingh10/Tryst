f6171ed5c015c06f149262f25d85452b06d0249c
Siddhartha singh <rajputsiddhartha2222@gmail.com>
2025-08-22 19:44:38 +0530

    login done

--- backend/user/src/controllers/user.ts
+++ backend/user/src/controllers/user.ts
@@ -1,42 +1,45 @@
+import { generateToken } from "../config/generateTokens.js";
 import { publishToQueue } from "../config/rabbitmq.js";
 import TryCatch from "../config/TryCatch.js";
 import { redisClient } from "../index.js";
 import { User } from "../model/User.js";
 
-export const loginUser=TryCatch(async(req,res)=>{
+export const loginUser = TryCatch(async (req, res) => {
+  const { email } = req.body;
 
-    const { email, password } = req.body;
+  const rateLimitKey = `otp:ratelimit:${email}`;
+  const rateLimit = await redisClient.get(rateLimitKey);
+  if (rateLimit) {
+    res.status(429).json({
+      message: "Too may requests. Please wait before requesting new opt",
+    });
+    return;
+  }
 
-    // Rate limiting logic
-    const rateLimitKey=`otp:rateLimit:${email}`;
-    const rateLimit=await redisClient.get(rateLimitKey);
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
 
-    if(rateLimit){
-        res.status(429).json({
-            message: "Too many requests. Please try again later."   
-        });
-        return;
-    }
-    //create OTP
-    const otp=Math.floor(100000 + Math.random() * 900000).toString();
-    const otpKey=`otp:${email}`;
-    await redisClient.set(otpKey, otp, 'EX', 300); // Store OTP for 5 minutes
-    await redisClient.set(rateLimitKey, '1', 'EX', 60); // Set rate limit for 1 minute
+  const otpKey = `otp:${email}`;
+  await redisClient.set(otpKey, otp, {
+    EX: 300,
+  });
 
-    // Publish OTP to RabbitMQ
+  await redisClient.set(rateLimitKey, "true", {
+    EX: 60,
+  });
+
   const message = {
     to: email,
-    subject: "Your OTP Code",
-    body: `Your OTP code is ${otp}. It is valid for 5 minutes.`
+    subject: "Your otp code",
+    body: `Your OTP is ${otp}. It is valid for 5 minutes`,
+  };
 
-  }
   await publishToQueue("send-otp", message);
 
-    res.status(200).json({
-        message: "OTP sent successfully"
-    });
+  res.status(200).json({
+    message: "OTP sent to your mail",
+  });
+});
 
-})
 
 // Controller function to verify a user using email and OTP
 export const verifyUser = TryCatch(async (req, res) => {
--- backend/user/src/routes/user.ts
+++ backend/user/src/routes/user.ts
@@ -1,5 +1,15 @@
 import express from 'express';
-import { loginUser } from '../controllers/user.js';
+import { loginUser,verifyUser,myProfile} from '../controllers/user.js';
 const router =express.Router();
+
+
+
 router.post('/login',loginUser);
+
+router.post("/verify", verifyUser);
+router.get("/me", isAuth, myProfile);
+
+
+
+
 export default router;    
